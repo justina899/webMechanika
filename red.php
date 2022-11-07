81c595118d2840a823327a4bfc7ded80fab1e27e
Justina <justina.babrauskiene@gmail.com>
2022-11-07 21:18:44 +0200

    blue red 5 uzd

---
+++ red.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="red.php">to red</a>
+<?php 
+    //  header('Location: blue.php');
+    //  exit();
+    header( "refresh:5;url=blue.php" );
+?>
+</body>
+</html>
