a044f2d87ae4e4c2b4265bf9c76bc68ec5f634c3
Justina <justina.babrauskiene@gmail.com>
2022-11-08 15:16:23 +0200

    9 done

---
+++ 9uzd.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_POST['boxes'])){
+    echo count($_POST['boxes']);
+}
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $color = "black";
+        $color2 = "white";
+}elseif($_SERVER["REQUEST_METHOD"] == "POST"){
+        $color = "white";
+        $color2 = "black";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color:<?= $color ?>; color:<?= $color2 ?>"> 
+<?php if($_SERVER["REQUEST_METHOD"] == "GET"){ ?>
+    <form action="" method="post">
+    <?php
+        $checkbox = '<input type="checkbox" name="boxes[]" id="">';
+        $letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+        for ($i = 0; $i <= rand(3,10); $i++){
+            echo $checkbox . $letters[$i];
+        }
+        ?>
+
+        <button type="submit">button</button>
+    </form>
+<?php
+}
+?>
+
+</body>
+</html>
