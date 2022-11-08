da870e8368b668b9b6e5877a5a10afdaee49b314
Justina <justina.babrauskiene@gmail.com>
2022-11-08 13:01:39 +0200

    8 done

---
+++ rose.php
@@ -0,0 +1,17 @@
+<?php
+header("Location: ./pink.php");
+die;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color:#FF0080">
+
+</body>
+</html>
